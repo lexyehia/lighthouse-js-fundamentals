10e95f2a138d8b57c45905f855b86368cc4da1a3
Chad Yehia <lex.yehia@icloud.com>
2017-09-10 01:07:45 +0000

    Fixed loops

--- ingredients.js
+++ ingredients.js
@@ -2,14 +2,14 @@ let ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powd
 
 // Write a while loop that prints out the contents of ingredients:
 let ingredient = 0;
-while (ingredient <= ingredients.length) {
+while (ingredient < ingredients.length) {
     console.log(ingredients[ingredient]);
     ingredient++;
 }
 
 // Write a for loop that prints out the contents of ingredients:
 
-for (var i = 0; i <= ingredients.length; i++) {
+for (var i = 0; i < ingredients.length; i++) {
     console.log(ingredients[i]);
 }
 
